2d0165886bac898154ad0a1a0a4c315513c7cb5d
Dev Sharma <devsharmaelc@gmail.com>
2023-12-22 02:28:49 +0530

    add left Side Bar

--- src/components/Left_side_Bar/Left_side_Bar.jsx
+++ src/components/Left_side_Bar/Left_side_Bar.jsx
@@ -16,9 +16,15 @@ import "./Left_side_Bar.css";
 
 const Left_side_Bar = () => {
   const [topicsshow, setTopicsShow] = useState(true);
+  const [gameShow, setGameShow] = useState(false);
+  const [sportsShow, setSportsShow] = useState(false);
+  const [businessShow, setBusinessShow] = useState(false);
+  const [cryptoShow, setCryptoShow] = useState(false);
+  const [tvShow, setTVShow] = useState(false);
+  const [celebrityShow, setCelebrityShow] = useState(false);
   return (
     <section className="main-container">
-      <div className="leftSide_bar">
+      <div className="leftSide_bar" id="leftSideBar_id">
         <div className="left-side-bar-top-content">
           <div className="left-name-icons">
             <FontAwesomeIcon className="icons-left-side" icon={faHouse} />
@@ -50,7 +56,12 @@ const Left_side_Bar = () => {
           </div>
           {topicsshow && (
             <div>
-              <div className="leftBar-iconsand-topics">
+              <div
+                className="leftBar-iconsand-topics"
+                onClick={() => {
+                  setGameShow((old) => !old);
+                }}
+              >
                 <div className="left-name-icons">
                   <FontAwesomeIcon
                     className="icons-left-side"
@@ -58,12 +69,37 @@ const Left_side_Bar = () => {
                   />
                   <p>Gaming</p>
                 </div>
-
                 <div className="arroow-icons">
-                  <FontAwesomeIcon icon={faAngleUp} />
+                  {!gameShow ? (
+                    <FontAwesomeIcon icon={faAngleDown} />
+                  ) : (
+                    <FontAwesomeIcon icon={faAngleUp} />
+                  )}
                 </div>
               </div>
-              <div className="leftBar-iconsand-topics">
+              {gameShow && (
+                <div className="left-side-bar">
+                  <div className="left-side1">
+                    <p>Valheim</p>
+                    <p>Genshim</p>
+                    <p>Minecraft</p>
+                    <p>Pokimane</p>
+                    <p>Halo Infinite</p>
+                    <p>Call of Duty: Warzone</p>
+                    <p>Path of Exile</p>
+                    <p>Hollow Knight: Siksong</p>
+                    <p>Escape from Tarkov</p>
+                    <p>Watch Dogs: Legion</p>
+                  </div>
+                </div>
+              )}
+
+              <div
+                className="leftBar-iconsand-topics"
+                onClick={() => {
+                  setSportsShow((old) => !old);
+                }}
+              >
                 <div className="left-name-icons">
                   <FontAwesomeIcon
                     className="icons-left-side"
@@ -72,10 +108,35 @@ const Left_side_Bar = () => {
                   <p>Sports</p>
                 </div>
                 <div className="arroow-icons">
-                  <FontAwesomeIcon icon={faAngleUp} />
+                  {sportsShow ? (
+                    <FontAwesomeIcon icon={faAngleUp} />
+                  ) : (
+                    <FontAwesomeIcon icon={faAngleDown} />
+                  )}
                 </div>
               </div>
-              <div className="leftBar-iconsand-topics">
+              {sportsShow && (
+                <div className="left-side-bar">
+                  <div className="left-side1">
+                    <p>NFL</p>
+                    <p>NBA</p>
+                    <p>Megan Anderson</p>
+                    <p>Atlanta Hawks</p>
+                    <p>Los Angeles Lakers</p>
+                    <p>Boston Celtics</p>
+                    <p>Arsenal F.C.</p>
+                    <p>Philadelphia 76ers</p>
+                    <p>Premier League</p>
+                    <p>UFC</p>
+                  </div>
+                </div>
+              )}
+              <div
+                className="leftBar-iconsand-topics"
+                onClick={() => {
+                  setBusinessShow((old) => !old);
+                }}
+              >
                 <div className="left-name-icons">
                   <FontAwesomeIcon
                     className="icons-left-side"
@@ -84,28 +145,100 @@ const Left_side_Bar = () => {
                   <p>Business</p>
                 </div>
                 <div className="arroow-icons">
-                  <FontAwesomeIcon icon={faAngleUp} />
+                  {businessShow ? (
+                    <FontAwesomeIcon icon={faAngleUp} />
+                  ) : (
+                    <FontAwesomeIcon icon={faAngleDown} />
+                  )}
                 </div>
               </div>
-              <div className="leftBar-iconsand-topics">
+              {businessShow && (
+                <div className="left-side-bar">
+                  <div className="left-side1">
+                    <p>GameStop</p>
+                    <p>Morderna</p>
+                    <p>Pfizer</p>
+                    <p>Johnson & Johnson</p>
+                    <p>Astrazeneca</p>
+                    <p>Walgreens</p>
+                    <p>Best Buy</p>
+                    <p>Novavax</p>
+                    <p>SpaceX</p>
+                    <p>Tesla</p>
+                  </div>
+                </div>
+              )}
+              <div
+                className="leftBar-iconsand-topics"
+                onClick={() => {
+                  setCryptoShow((old) => !old);
+                }}
+              >
                 <div className="left-name-icons">
                   <FontAwesomeIcon className="icons-left-side" icon={faGg} />
                   <p>Crpto</p>
                 </div>
                 <div className="arroow-icons">
-                  <FontAwesomeIcon icon={faAngleUp} />
+                  {cryptoShow ? (
+                    <FontAwesomeIcon icon={faAngleUp} />
+                  ) : (
+                    <FontAwesomeIcon icon={faAngleDown} />
+                  )}
                 </div>
               </div>
-              <div className="leftBar-iconsand-topics">
+              {cryptoShow && (
+                <div className="left-side-bar">
+                  <div className="left-side1">
+                    <p>Cardano</p>
+                    <p>Dogecoin</p>
+                    <p>Algorand</p>
+                    <p>Bitcoin</p>
+                    <p>Litecoin</p>
+                    <p>Basic Attention Token</p>
+                    <p>Bitcoin</p>
+                  </div>
+                </div>
+              )}
+              <div
+                className="leftBar-iconsand-topics"
+                onClick={() => {
+                  setTVShow((old) => !old);
+                }}
+              >
                 <div className="left-name-icons">
                   <FontAwesomeIcon className="icons-left-side" icon={faTv} />
                   <p>Television</p>
                 </div>
                 <div className="arroow-icons">
-                  <FontAwesomeIcon icon={faAngleUp} />
+                  {tvShow ? (
+                    <FontAwesomeIcon icon={faAngleUp} />
+                  ) : (
+                    <FontAwesomeIcon icon={faAngleDown} />
+                  )}
                 </div>
               </div>
-              <div className="leftBar-iconsand-topics">
+              {tvShow && (
+                <div className="left-side-bar">
+                  <div className="left-side1">
+                    <p>The Real Housewives of Atlanta</p>
+                    <p>The Bachelor</p>
+                    <p>Sister Wives</p>
+                    <p>The Amazing Race Australia</p>
+                    <p>Married at First Sight</p>
+                    <p>The Real Housewives of Dalias</p>
+                    <p>My 600-lb Life</p>
+                    <p>Last Week Tonight with John Oilver</p>
+                    <p>90 Day Fiance</p>
+                    <p>Wife Swap</p>
+                  </div>
+                </div>
+              )}
+              <div
+                className="leftBar-iconsand-topics"
+                onClick={() => {
+                  setCelebrityShow((old) => !old);
+                }}
+              >
                 <div className="left-name-icons">
                   <FontAwesomeIcon
                     className="icons-left-side"
@@ -114,9 +247,29 @@ const Left_side_Bar = () => {
                   <p>Celebrity</p>
                 </div>
                 <div className="arroow-icons">
-                  <FontAwesomeIcon icon={faAngleUp} />
+                  {celebrityShow ? (
+                    <FontAwesomeIcon icon={faAngleUp} />
+                  ) : (
+                    <FontAwesomeIcon icon={faAngleDown} />
+                  )}
                 </div>
               </div>
+              {celebrityShow && (
+                <div className="left-side-bar">
+                  <div className="left-side1">
+                    <p>Kim Kardashian</p>
+                    <p>Doja Cat</p>
+                    <p>Iggy Azalea</p>
+                    <p>Anya Taylor-Joy</p>
+                    <p>Jamie Lee Curtis</p>
+                    <p>Natalie Portman</p>
+                    <p>Henry Cavill</p>
+                    <p>Millile Bobby Brown</p>
+                    <p>Tom Hiddeston</p>
+                    <p>Keanu Reeves</p>
+                  </div>
+                </div>
+              )}
             </div>
           )}
         </div>
